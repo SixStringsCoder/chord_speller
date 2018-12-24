117388d790dbe6216f4e90a1d270f597d9392457
steve hanlon <rshanlon@gmail.com>
2018-12-24 01:59:56 -0800

    Add state to Note component and add method and ternary expression to alter border color based on correct or incorrect answer

--- src/components/Note/Note.js
+++ src/components/Note/Note.js
@@ -1,16 +1,42 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const Note = ({ note, id }) => {
-  return (
-      <input
-        type="text"
-        name={note}
-        id={id}
-        className="note"
-        tabIndex="0"
-        maxLength={note.length === 2 ? 2 : 1}
-      />
-  )
+class Note extends Component {
+  state = {
+    note: '',
+    correct: false,
+    border: 'red'
+  }
+
+  handleNote = (e) => {
+    console.log(e.target.name);
+    this.setState({
+      note: e.target.value,
+      correct: e.target.value === e.target.name ? true : false,
+    });
+  }
+
+  handleCorrect = (e) => {
+    if (this.state.correct) {
+      this.setState({ border: 'green' })
+    }
+  }
+
+  render() {
+    const {note, id } = this.props;
+    return (
+        <input
+          type="text"
+          value={this.state.note}
+          onChange={this.handleNote}
+          name={note}
+          id={id}
+          // Correct answer sets border color to green else red
+          className={!this.state.correct ? "note red" : "note green"}
+          tabIndex="0"
+          maxLength={note.length === 2 ? 2 : 1}
+        />
+    )
+  }
 }
 
 export default Note;
